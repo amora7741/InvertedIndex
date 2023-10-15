48168347f09df1af4862929cb0932cabf1e0ab6a
amora7741 <mora7741@gmail.com>
2023-10-15 14:42:59 -0700

    Added deletion of documents

--- db_connection.py
+++ db_connection.py
@@ -100,35 +100,31 @@ def createDocument(cur, docId, docText, docTitle, docDate, docCat):
             recset = [docId, term, count]
             cur.execute(query, recset)
 
-def createDocument(cur, docId, docText, docTitle, docDate, docCat):
-
-    # 1 Get the category id based on the informed category name
-    # --> add your Python code here
-
-    # 2 Insert the document in the database. For num_chars, discard the spaces and punctuation marks.
-    # --> add your Python code here
-
-    # 3 Update the potential new terms.
-    # 3.1 Find all terms that belong to the document. Use space " " as the delimiter character for terms and Remember to lowercase terms and remove punctuation marks.
-    # 3.2 For each term identified, check if the term already exists in the database
-    # 3.3 In case the term does not exist, insert it into the database
-    # --> add your Python code here
-
-    # 4 Update the index
-    # 4.1 Find all terms that belong to the document
-    # 4.2 Create a data structure the stores how many times (count) each term appears in the document
-    # 4.3 Insert the term and its corresponding count into the database
-    # --> add your Python code here
-
 def deleteDocument(cur, docId):
-
     # 1 Query the index based on the document to identify terms
-    # 1.1 For each term identified, delete its occurrences in the index for that document
-    # 1.2 Check if there are no more occurrences of the term in another document. If this happens, delete the term from the database.
-    # --> add your Python code here
+    query = "SELECT term FROM Index WHERE doc_number = %s"
+    recset = [docId]
+    cur.execute(query, recset)
+    terms = [item['term'] for item in cur.fetchall()]
+
+    for term in terms:
+        # 1.1 For each term identified, delete its occurrences in the index for that document
+        query = "DELETE FROM Index WHERE doc_number = %s AND term = %s"
+        recset = [docId, term]
+        cur.execute(query, recset)
+        
+        # 1.2 Check if there are no more occurrences of the term in another document. If this happens, delete the term from the database.
+        query = "SELECT * FROM Index WHERE term = %s"
+        recset = [term]
+        cur.execute(query, recset)
+        if cur.fetchone() is None:
+            query = "DELETE FROM Terms WHERE term = %s"
+            cur.execute(query, recset)
 
     # 2 Delete the document from the database
-    # --> add your Python code here
+    query = "DELETE FROM Documents WHERE doc_number = %s"
+    recset = [docId]
+    cur.execute(query, recset)
 
 def updateDocument(cur, docId, docText, docTitle, docDate, docCat):
 
