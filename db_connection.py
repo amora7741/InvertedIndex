5aef49815d0f0f42a640c8c79a5dd940349aa2f4
amora7741 <mora7741@gmail.com>
2023-10-15 13:47:02 -0700

    Added creation of documents

--- db_connection.py
+++ db_connection.py
@@ -37,7 +37,6 @@ def connectDataBase():
 
 
 def createCategory(cur, catId, catName):
-
     # Insert a category in the database
     try:
         query = "INSERT INTO Categories (category_id, name) VALUES (%s, %s)"
@@ -45,6 +44,61 @@ def createCategory(cur, catId, catName):
         cur.execute(query, recset)
     except Exception as e:
         print(f"Error creating category: {e}")
+
+def createDocument(cur, docId, docText, docTitle, docDate, docCat):
+    # 1 Get the category id based on the informed category name
+    query = "SELECT category_id FROM Categories WHERE name = %s"
+    recset = [docCat]
+    cur.execute(query, recset)
+    cat_id = cur.fetchone()['category_id']
+    
+    # 2 Insert the document in the database. For num_chars, discard the spaces and punctuation marks.
+    numChars = len(''.join(filter(str.isalnum, docText)))
+    query = "INSERT INTO Documents (doc_number, text, title, date, num_chars, category_id) VALUES (%s, %s, %s, %s, %s, %s)"
+    recset = [docId, docText, docTitle, docDate, numChars, cat_id]
+    cur.execute(query, recset)
+
+    # 3 Update the potential new terms.
+
+    # 3.1 Find all terms that belong to the document. Use space " " as the
+    # delimiter character for terms and Remember to lowercase terms and remove
+    # punctuation marks.
+    filteredChars = filter(lambda x: x.isalnum() or x.isspace(), docText)
+    cleanedString = ''.join(filteredChars)
+    lowercaseString = cleanedString.lower()
+    terms = lowercaseString.split()
+
+    for term in set(terms):
+        # 3.2 Check if the term already exists in the database
+        query = "SELECT term FROM Terms WHERE term = %s"
+        recset = [term]
+        cur.execute(query, recset)
+        if cur.fetchone() is None:
+            # 3.3 In case the term does not exist, insert it into the database
+            query = "INSERT INTO Terms (term, num_chars) VALUES (%s, %s)"
+            recset = [term, len(term)]
+            cur.execute(query, recset)
+
+    # 4 Update the index
+    termCount = {}
+    for term in terms:
+        termCount[term] = termCount.get(term, 0) + 1
+
+    for term, count in termCount.items():
+        query = "SELECT count FROM Index WHERE doc_number = %s AND term = %s"
+        recset = [docId, term]
+        cur.execute(query, recset)
+        result = cur.fetchone()
+
+        if result:
+            newCount = result['count'] + count
+            query = "UPDATE Index SET count = %s WHERE doc_number = %s AND term = %s"
+            recset = [newCount, docId, term]
+            cur.execute(query, recset)
+        else:
+            query = "INSERT INTO Index (doc_number, term, count) VALUES (%s, %s, %s)"
+            recset = [docId, term, count]
+            cur.execute(query, recset)
 
 def createDocument(cur, docId, docText, docTitle, docDate, docCat):
 
